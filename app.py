1212e355515d8bcb3f3b4e8fa71bff8f57feb7dd
gabrielbdavila <gabriel.davila@physa.ai>
2025-10-17 15:16:14 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from dash import Dash, html
+
+# Inicializa o app
+app = Dash(__name__)
+
+# Define o layout com a mensagem
+app.layout = html.Div(
+    children=[
+        html.H1("Hello Time Tech&Inovação")
+    ]
+)
+
+# Roda o servidor
+if __name__ == "__main__":
+    app.run_server(debug=True)
